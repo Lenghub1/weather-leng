cbf3fe9d3629ac262990d75b4f7e8ea17de4f233
Tourleng <hourleng9@gmail.com>
2023-09-11 16:30:22 +0700

    Search

--- src/component/Search.jsx
+++ src/component/Search.jsx
@@ -1,12 +1,48 @@
+import React, { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
 import './search.css';
+import cityList from '../city_data.json'; 
 
-const Search = () => {
+const Search = ({ onSearchChange }) => {
+  const [search, setSearch] = useState(null);
+
+  const loadOptions = (inputValue) => {
+    
+    if (inputValue.length < 3) {
+      return Promise.resolve({ options: [] }); 
+    }
+
+    const filteredCities = cityList.filter((city) =>
+      city.name.toLowerCase().includes(inputValue.toLowerCase())
+    );
+
+    const options = filteredCities.map((city) => ({
+      value: `${city.coord.lat} ${city.coord.lon}`,
+      label: `${city.name}, ${city.country_name}`,
+      lat: city.coord.lat,
+      lon: city.coord.lon
+    }));
+
+    return Promise.resolve({
+      options,
+      hasMore: false, 
+    });
+  };
+
+  const handleOnChange = (searchData) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+  };
+
   return (
     <div>
       <AsyncPaginate
         className="search-input"
         placeholder="Search for city"
+        debounceTimeout={600}
+        value={search}
+        onChange={handleOnChange}
+        loadOptions={loadOptions}
       />
     </div>
   );
