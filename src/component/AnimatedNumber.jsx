33e3c7f224a936028d026cf7f62a314fdd0919fc
Tourleng <hourleng9@gmail.com>
2023-09-14 15:41:48 +0700

    background change when condition change function animatenumber change border scatter.svg and mist.svg

---
+++ src/component/AnimatedNumber.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+
+const AnimatedNumber = ({ value, duration }) => {
+  const [displayValue, setDisplayValue] = useState(0);
+
+  useEffect(() => {
+    let startTimestamp;
+    const updateNumber = (timestamp) => {
+      if (!startTimestamp) startTimestamp = timestamp;
+      const progress = timestamp - startTimestamp;
+      const percentage = Math.min(progress / duration, 1);
+      const animatedValue = Math.round(value * percentage);
+
+      setDisplayValue(animatedValue);
+
+      if (percentage < 1) {
+        requestAnimationFrame(updateNumber);
+      }
+    };
+    
+    requestAnimationFrame(updateNumber);
+
+  }, [value, duration]);
+
+  return <span>{displayValue}°C</span>;
+};
+
+export default AnimatedNumber;
